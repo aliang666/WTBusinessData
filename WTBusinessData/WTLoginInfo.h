a0ecafdd1934fa5ead6a49df01e678230a856a39
计恩良 <454309007@qq.com>
2018-05-27 17:26:09 +0800

    修改登录数据
    修改登录数据

--- WTBusinessData/WTLoginInfo.h
+++ WTBusinessData/WTLoginInfo.h
@@ -1,4 +1,18 @@
 #import <Foundation/Foundation.h>
 @interface WTLoginInfo : NSObject{
 }
+@property (nonatomic,copy) NSString *address;
+@property (nonatomic,copy) NSString *age;
+@property (nonatomic,copy) NSString *authorize;
+@property (nonatomic,copy) NSString *birthday;
+@property (nonatomic,copy) NSString *email;
+@property (nonatomic,copy) NSString *id;
+@property (nonatomic,copy) NSString *industry;
+@property (nonatomic,copy) NSString *nickname;
+@property (nonatomic,copy) NSString *occupation;
+@property (nonatomic,copy) NSString *phoneNum;
+@property (nonatomic,copy) NSString *remainLicenseDays;
+@property (nonatomic,copy) NSString *sex;
+@property (nonatomic,copy) NSString *iMLogin;
++ (instancetype)shareInstance
 @end
--- WTBusinessData/WTLoginInfo.m
+++ WTBusinessData/WTLoginInfo.m
@@ -1,4 +1,13 @@
 #import "WTLoginInfo.h"
 
 @implementation WTLoginInfo
++ (instancetype)shareInstance
+{
+    static WTLoginInfo *share = nil ;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        share = [[WTLoginInfo alloc]init];
+    });
+    return share;
+}
 @end
