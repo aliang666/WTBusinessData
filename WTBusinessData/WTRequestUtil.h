5deef3681a89c5c2544aae0342724593e07f26c9
elji <elji@iflytek.com>
2018-06-14 14:02:30 +0800

    1
    1

--- WTBusinessData/WTRequestUtil.h
+++ WTBusinessData/WTRequestUtil.h
@@ -1,10 +1,14 @@
 #import <Foundation/Foundation.h>
 #import "WTHttpUtil.h"
-#define WT_APP_Host  @"http://itma.openspeech.cn:8088"
+#define WT_APP_Host  @"http://itma.openspeech.cn:8088/itmaDevice/"
 
-#define WT_APP_Login_Path   @"/itmaDevice/UserLoginCMCC.do"
+#define WT_APP_Login_Path   @"UserLoginCMCC.do"
+#define WT_APP_UpdateUserInfo_Path   @"UpdateUserInfoCMCC,.do"
 
 @interface WTRequestUtil : NSObject{
 }
+//用户登录接口
 + (void)loginByPhone:(NSString *)phone password:(NSString *)pass loginType:(int)loginType success:(WTHttpRequestSuccess)success failure:(WTHttpRequestFailed)failure;
+//更新用户资料接口
++ (void)myInfoUpdateRequest:(NSString *)fieldName fieldValue:(NSString *)fieldValue loginType:(int)loginType userId:(NSString *)uuid success:(WTHttpRequestSuccess)success failure:(WTHttpRequestFailed)failure;
 @end
--- WTBusinessData/WTRequestUtil.m
+++ WTBusinessData/WTRequestUtil.m
@@ -1,6 +1,7 @@
 #import "WTRequestUtil.h"
 #import "NSString+Category.h"
 #import "WTUtil.h"
+
 @implementation WTRequestUtil
 + (void)loginByPhone:(NSString *)phone password:(NSString *)pass loginType:(int)loginType success:(WTHttpRequestSuccess)success failure:(WTHttpRequestFailed)failure {
     NSMutableDictionary *param = [NSMutableDictionary dictionary];
@@ -13,10 +14,27 @@
         if ([[WTUtil strRelay:responseObject[@"code"]] isEqualToString:@"1000"]) {
             success(responseObject);
         } else {
-            
+            failure(responseObject);
         }
     } failure:^(NSError *error) {
-        NSLog(@"bbbbbbb");
+        failure(error);
+    }];
+}
+
++ (void)myInfoUpdateRequest:(NSString *)fieldName fieldValue:(NSString *)fieldValue loginType:(int)loginType userId:(NSString *)uuid success:(WTHttpRequestSuccess)success failure:(WTHttpRequestFailed)failure {
+    NSMutableDictionary *param = [NSMutableDictionary dictionary];
+    [param setObject:fieldName forKey:@"fieldName"];
+    [param setObject:fieldValue forKey:@"fieldValue"];
+    [param setObject:[NSNumber numberWithInt:loginType] forKey:@"accountType"];
+    [param setObject:uuid forKey:@"uuid"];
+    
+    [WTHttpUtil POSTWithURL:[NSString stringWithFormat:@"%@%@",WT_APP_Host,WT_APP_UpdateUserInfo_Path] parameters:param success:^(id responseObject) {
+        if ([[WTUtil strRelay:responseObject[@"code"]] isEqualToString:@"1000"]) {
+            success(responseObject);
+        } else {
+            failure(responseObject);
+        }
+    } failure:^(NSError *error) {
         failure(error);
     }];
 }
